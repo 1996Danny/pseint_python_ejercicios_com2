9e46700c436326abcc98685d128e00edfc4d5e17
Daniel Frias <friasdaniel996@gmail.com>
2025-11-10 19:44:35 -0300

    ejercitacion

--- python/08-semana/ejercicio_agenda/conexion_db.py
+++ python/08-semana/ejercicio_agenda/conexion_db.py
@@ -10,28 +10,42 @@ def conexion():
     ))
 
 # - Añadir contacto
-con = conexion()
-print("conexion establecida")
-cursor = con.cursor()
-cursor.execute(
-                "insert into agenda(nombre, apellido, telefono, email) values (%s,%s,%s,%s)", \
-                ("Ramon", "Perez", "12423523", "ramon@perez.com")
-                )
+# con = conexion()
+# print("conexion establecida")
+# cursor = con.cursor()
+# cursor.execute(
+#                 "insert into agenda(nombre, apellido, telefono, email) values (%s,%s,%s,%s)", \
+#                 ("Ramon", "Perez", "12423523", "ramon@perez.com")
+#                 )
 
-con.commit()
+# con.commit()
 
-print(f"Datos insertados: {cursor.rowcount}")
+# print(f"Datos insertados: {cursor.rowcount}")
 
-con.close()
+# con.close()
 
 # - Borrar contacto ()
+# con = conexion()
+# print("conexion establecida")
+# cursor = con.cursor()
+# cursor.execute("delete from agenda where=3")
+# con.commit()
+# print(f"Datos elimindas: {cursor.rowcount}")
+# con.close()
 
 # - Listar contactos(todos los contactos)
+# select * from contactos
 
-# - Buscar contacto (por id, nombre)
+# - Buscar contacto (por id, nombre) con id=5
+# select * from contacto where id=5
 
 # - Editar contacto ()
 
+# con = conexion()
+# print("conexion establecida")
+# cursor = con.cursor()
+# cursor.execute("update agenda set nombre='Ramiro' where id=2")
+# con.commit()
 
-
-
+# # print(f"Datos insertados: {cursor.rowcount}")
+# con.close()
